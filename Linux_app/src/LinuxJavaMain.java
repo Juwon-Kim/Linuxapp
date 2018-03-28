c21f293bc0b5a5b7f4b9da72cb76ef0d4cedb039
BIT <BIT@DESKTOP-DOGVD1T>
2018-03-28 15:21:34 +0900

    2

--- Linux_app/src/LinuxJavaMain.java
+++ Linux_app/src/LinuxJavaMain.java
@@ -2,5 +2,8 @@
 public class LinuxJavaMain {
 	public static void main(String[] args) {
 		System.out.println("=====HELLO LINUX=====");
+		System.out.println("=====HELLO LINUX=====");
+		System.out.println("=====HELLO LINUX=====");
+		System.out.println("=====HELLO LINUX=====");
 	}
 }
